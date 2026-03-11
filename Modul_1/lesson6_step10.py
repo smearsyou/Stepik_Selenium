e3a8e40edb873e7931fb6b8b1a2e542390d88adc
smearsyou <archakovma@mail.ru>
2026-03-11 15:30:43 +0300

    Update lesson6_step10.py

--- Modul_1/lesson6_step10.py
+++ Modul_1/lesson6_step10.py
@@ -1,10 +1,4 @@
-# MODUL 1. LESSON 6. TASK 10
 
-# Проверьте, что можно зарегистрироваться на сайте, заполнив только обязательные поля,
-# отмеченные символом *: First name, last name, email. Текст для полей может быть любым.
-# Успешность регистрации проверяется сравнением ожидаемого текста "Congratulations! You have successfully registered!"
-# с текстом на странице, которая открывается после регистрации.
-# Для сравнения воспользуемся стандартной конструкцией assert из языка Python.
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -15,7 +9,6 @@ try:
     browser = webdriver.Chrome()
     browser.get(link)
 
-    # Код, который заполняет обязательные поля
     input1 = browser.find_element(By.CSS_SELECTOR, "input.form-control.first[required]")
     input1.send_keys("Anastasiya")
     input2 = browser.find_element(By.CSS_SELECTOR, "input.form-control.second[required]")
@@ -23,24 +16,20 @@ try:
     input3 = browser.find_element(By.CSS_SELECTOR, "input.form-control.third")
     input3.send_keys("ABmail@mail.ru")
 
-    # Отправляем заполненную форму
+
     button = browser.find_element(By.CSS_SELECTOR, "button.btn")
     button.click()
 
-    # Проверяем, что смогли зарегистрироваться
-    # ждём загрузки страницы
+
     time.sleep(1)
 
-    # находим элемент, содержащий текст
-    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
-    # записываем в переменную welcome_text текст из элемента welcome_text_elt
-    welcome_text = welcome_text_elt.text
 
-    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+
+    welcome_text = welcome_text_elt.text
+йта
     assert "Congratulations! You have successfully registered!" == welcome_text
 
 finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
     time.sleep(5)
-    # закрываем браузер после всех манипуляций
     browser.quit()
